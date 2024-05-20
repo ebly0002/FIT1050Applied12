3591968fa698144ff17b353dc04b68f7df937ee5
ebly0002 <114212058+ebly0002@users.noreply.github.com>
2024-05-20 12:54:31 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function showMessage() {
+    alert('Javascript!');
+}
+
+document.querySelector('h1').addEventListener("click", showMessage);
+
+
+Fancybox.bind("[data-fancybox]")
+
+
+$("button").click(filterItems)
+
+function filterItems() {
+    let button = this;
+    let selector = $(button).data("filter");
+    console.log(selector);
+
+    $("#products img").not(selector).hide(500);
+    $(selector).show(500);
+}
